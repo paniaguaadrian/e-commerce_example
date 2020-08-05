2f1513bb5fc13ea77e58d476563798f0ec73fd9a
paniaguaadrian <paniaguasanchezadrian@gmail.com>
2020-08-05 19:47:49 +0200

    e-commerce website.

--- app/js/script.js
+++ app/js/script.js
@@ -4,22 +4,22 @@
 
 // Array for items
 let items = [{
-        name: 'Ironhack T',
+        name : 'Ironhack T',
         price: 10,
         image: 'https://miro.medium.com/max/5190/1*aVsUjp1zvlRb1799gDjbLA@2x.jpeg'
     },
     {
-        name: 'Ironhack Hoodie',
+        name : 'Ironhack Hoodie',
         price: 15,
         image: 'https://m.media-amazon.com/images/I/B1i3u9-Q-KS._AC_CLa%7C2140%2C2000%7CB1wqstnnTfS.png%7C0%2C0%2C2140%2C2000%2B0.0%2C0.0%2C2140.0%2C2000.0_UL1500_.png'
     },
     {
-        name: 'Ironhack Sticker',
+        name : 'Ironhack Sticker',
         price: 2,
         image: 'https://e7.pngegg.com/pngimages/887/803/png-clipart-ironhack-web-development-job-startup-company-design-blue-user-interface-design-thumbnail.png'
     },
     {
-        name: 'Ironhack Mug',
+        name : 'Ironhack Mug',
         price: 8,
         image: 'https://d0bb7f9bf11b5ad1a6b2-6175f06f5e3f64e15abbf67415a276ec.ssl.cf1.rackcdn.com/product-images/designlab/11-oz-traditional-ceramic-coffee-mugs-7102-white1582888132.jpg'
     },
@@ -34,10 +34,10 @@ let list = document.querySelector('#items');
 
 items.forEach((item, i) => {
     list.innerHTML += `<li>
-    <div>Name: ${item.name}</div> 
+    <div>Name : ${item.name}</div>
     <div>Price: $${item.price}</div>
-    <image src='${item.image}'/>
-    <input type='number' placeholder='Quantity' onchange='inputChange(${i}, 
+    <image src  = '${item.image}'/>
+    <input type = 'number' placeholder = 'Quantity' onchange = 'inputChange(${i}, 
     "${item.name}", "${item.price}", "${item.image}")
     '/>
     <button>Buy Item</button>
@@ -46,15 +46,15 @@ items.forEach((item, i) => {
 
 
 function showCart() {
-    let cartItems = document.querySelector('#cart');
-    let grandTotal = 0;
-    cartItems.innerHTML = ''
+    let cartItems           = document.querySelector('#cart');
+    let grandTotal          = 0;
+        cartItems.innerHTML = ''
     cart.forEach((item, i) => {
-        grandTotal += item.price * item.quantity
+        grandTotal          += item.price * item.quantity
         cartItems.innerHTML += `<li>
-    <div>Name: ${item.name}</div> 
+    <div>Name    : ${item.name}</div>
     <div>Quantity: ${item.quantity}</div>
-    <image src='${item.image}'/>
+    <image src = '${item.image}'/>
     </li>`
     })
 
@@ -66,15 +66,15 @@ function showCart() {
 function inputChange(i, name, price, image) {
     console.log('I want to buy the ', i, ' item named', name, 'that costs $', price)
     let listItem = document.querySelectorAll('li')[i]
-    let input = listItem.querySelector('input')
-    let button = listItem.querySelector('button')
+    let input    = listItem.querySelector('input')
+    let button   = listItem.querySelector('button')
 
     button.onclick = function () {
         cart.push({
             quantity: input.value,
-            name: name,
-            price: price,
-            image: image
+            name    : name,
+            price   : price,
+            image   : image
         })
         console.log(cart)
         showCart()
